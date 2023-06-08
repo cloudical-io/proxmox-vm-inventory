1db16fe3953f370064986f616df470839e84322e
Daniel Lehmann <daniel.lehmann@cloudical.io>
2023-06-08 12:51:54 +0000

    dev to main

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"io"
 	"os"
-	"strings"
 
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/charmbracelet/log"
@@ -14,8 +13,9 @@ import (
 type Config struct {
 	RequestTimeout    int       `mapstructure:"timeout"`
 	FetchInterval     int       `mapstructure:"fetchInterval"`
-	Clusters          []Cluster `mapstructure:"clusters"`
 	HttpListenAddress string    `mapstructure:"httpAddress"`
+	LogLevel          string    `mapstructure:"logLevel"`
+	Clusters          []Cluster `mapstructure:"clusters"`
 }
 
 type Cluster struct {
@@ -26,57 +26,56 @@ type Cluster struct {
 }
 
 // variables to be parsed
-var (
-	filePath = kingpin.
-			Flag("config-file", "YAML file containing your config values. Values set here override all commandline flags and environment vars").
-			Short('f').
-			Required().
-			Envar("INV_CONFIG_FILE").
-			String()
-	logLevel = kingpin.
-			Flag("log-level", "Set the Log Level / verbosity").
-			Short('L').
-			Envar("INV_LOG_LEVEL").
-			Default("INFO").
-			Enum("DEBUG", "INFO", "WARN", "ERROR", "FATAL")
-	requestTimeout = kingpin.
-			Flag("timeout", "Time in seconds before a request times out").
-			Short('t').
-			Default("10").
-			Envar("INV_TIMEOUT").
-			Int()
-	fetchInterval = kingpin.
-			Flag("fetch-interval", "Interval at whicht to refetch all VMs").
-			Short('i').
-			Default("300").
-			Envar("INV_INTERVAL").
-			Int()
-	httpListenAddress = kingpin.
-				Flag("listen-address", "The http port to listen on").
-				HintOptions(":8080", "127.0.0.1:8080", "[::]:8080").
-				Default(":8080").
-				Short('l').
-				Envar("INV_HTTP_LISTEN").
-				String()
-)
+var filePath string = *kingpin.
+	Flag("config-file", "YAML file containing your config values. Values set here override all commandline flags and environment vars").
+	Short('f').
+	Required().
+	Envar("INV_CONFIG_FILE").
+	String()
 
+var c *Config = &Config{
+	FetchInterval: *kingpin.
+		Flag("fetch-interval", "Interval at whicht to refetch all VMs").
+		Short('i').
+		Default("300").
+		Envar("INV_INTERVAL").
+		Int(),
+	RequestTimeout: *kingpin.
+		Flag("timeout", "Time in seconds before a request times out").
+		Short('t').
+		Default("10").
+		Envar("INV_TIMEOUT").
+		Int(),
+	HttpListenAddress: *kingpin.
+		Flag("listen-address", "The http port to listen on").
+		HintOptions(":8080", "127.0.0.1:8080", "[::]:8080").
+		Default(":8080").
+		Short('l').
+		Envar("INV_HTTP_LISTEN").
+		String(),
+	LogLevel: *kingpin.
+		Flag("log-level", "Set the Log Level / verbosity").
+		Short('L').
+		Envar("INV_LOG_LEVEL").
+		Default("INFO").
+		Enum("DEBUG", "INFO", "WARN", "ERROR", "FATAL"),
+}
+
+// parse values and set loglevel
 func init() {
 	kingpin.Parse()
 
-	s := strings.ToLower(*logLevel)
-	l := log.ParseLevel(s)
-	log.SetLevel(l)
-	log.Info("log level", "level", log.GetLevel().String())
+	// setting log level for library
+	level := log.ParseLevel(c.LogLevel)
+	log.SetLevel(level)
+	log.Info("Setting Log Level", "level", c.LogLevel)
 }
 
+// Creates a new Config file
+// reads commandline flags and parses them
+// any values in the provided yaml overwrite CLI flags
 func New() *Config {
-	c := &Config{
-		FetchInterval:     *fetchInterval,
-		RequestTimeout:    *requestTimeout,
-		HttpListenAddress: *httpListenAddress,
-	}
-
-	f, err := os.Open(*filePath)
+	f, err := os.Open(filePath)
 	defer func(f *os.File) {
 		if err := f.Close(); err != nil {
 			log.Error("closing file failed", "err", err)
@@ -92,11 +91,13 @@ func New() *Config {
 		log.Fatal("cold not read content", "err", err)
 	}
 
+	// unmarshal yaml into interface
 	var y interface{}
 	if err := yaml.Unmarshal(r, &y); err != nil {
 		log.Fatal("cold not read YAML", "err", err)
 	}
 
+	// decoding yaml into struct
 	decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{WeaklyTypedInput: true, Result: &c})
 	if err := decoder.Decode(y); err != nil {
 		log.Fatal("error unmarshalling YAML", "err", err)
